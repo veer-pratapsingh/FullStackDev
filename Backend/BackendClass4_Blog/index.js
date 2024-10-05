cbb3bb44c771171bb148777b8590c95eaa522ceb
[veer singh] <[veerrpratapsingh@gmail.com]>
2024-10-05 16:07:36 +0530

    Update

--- Backend/BackendClass4_Blog/index.js
+++ Backend/BackendClass4_Blog/index.js
@@ -6,6 +6,9 @@ const PORT = process.env.PORT || 3001;
 
 app.use(express.json());
 
+const todoRoutes = require("./routes/todos");
+app.use("/api/v1", todoRoutes);
+
 app.listen(PORT, () => {
     console.log("Server Connected");
 })
